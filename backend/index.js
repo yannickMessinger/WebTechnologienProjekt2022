dbb439eee9b45559b33b8cce61e9ca8dec376ded
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-13 07:54:21 +0100

    mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from "express" //Import the express dependency
+import mongoose from "mongoose"
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 4000;                  //Save the port number where your server will be listening
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/quiz', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection establshed successfully");
+})
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
