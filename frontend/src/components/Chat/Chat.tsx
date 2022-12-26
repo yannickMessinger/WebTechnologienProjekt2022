8d9c244b4f0960f3aa3d95507b419815c224784d
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-26 15:04:58 +0100

    formatting

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,45 +1,43 @@
-import React, { useState } from 'react'
-import { useChat } from '../../hooks/useChat';
-import { ChatMsgList } from './ChatMsgList';
+import React, { useState } from "react";
+import { useChat } from "../../hooks/useChat";
+import { ChatMsgList } from "./ChatMsgList";
 import css from "./Chatstyle.module.css";
 
-
 export const Chat = () => {
-  const {sendChatMsg,msglist} = useChat();
-  
+  const { sendChatMsg, msglist } = useChat();
 
-  
-
-  const [messageContent, setMessageContent] = useState("")
- 
-  
- 
-  
-  
-  
- 
-  
-  
+  const [messageContent, setMessageContent] = useState("");
 
   const sendMessage = () => {
-  sendChatMsg({date:new Date(), content:messageContent})
-  
-  setMessageContent("");  
-  }
+    sendChatMsg({ date: new Date(), content: messageContent });
+
+    setMessageContent("");
+  };
 
   return (
     <div>
       <h3>CHAT</h3>
-      <input placeholder="message" value={messageContent} onChange={(e) =>{setMessageContent(e.target.value)}}></input>
-      <button onClick={() => {sendMessage()}}>send message</button>
+      <input
+        placeholder="message"
+        value={messageContent}
+        onChange={(e) => {
+          setMessageContent(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          sendMessage();
+        }}
+      >
+        send message
+      </button>
       <div>
         <ul>
-        {msglist.map((msg) =>{
-          return <li>{msg.content}</li>
-        })}
-      </ul>
+          {msglist.map((msg) => {
+            return <li>{msg.content}</li>;
+          })}
+        </ul>
       </div>
     </div>
-    
-  )
-}
+  );
+};
