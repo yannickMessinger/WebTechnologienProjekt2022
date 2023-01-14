2ca2f1eabb9fadf61ffe8e6f854a67d906e8af40
Felix Hammer <felix.hammer@student.hs-rm.de>
2023-01-14 18:52:45 +0100

    Login-Methode

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,46 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  async function handleSubmit(e: any) {
+    e.preventDefault()
+
+    const body = {
+      username: e.currentTarget.username.value,
+      password: e.currentTarget.password.value,
+    }
+
+    try {
+      const res = await fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      if (res.status === 200) {
+        navigate("/");
+      } else {
+        throw new Error(await res.text())
+      }
+    } catch (error) {
+      console.error('An unexpected error happened occurred:', error)
+    }
+  }
+
   return (
     <>
-    <label>
-        Nutzername
-        <input id="username"></input>
-    </label>
-    <label>
-        Passwort
-        <input id="passwort" type="password"></input>
-    </label>
-    <button>Login</button>
-    <button>Login mit Google</button>
-    <button>Registrieren</button>
+    <h1>Login</h1>
+      <label>
+          Nutzername
+          <input id="username"></input>
+      </label>
+      <label>
+          Passwort
+          <input id="passwort" type="password"></input>
+      </label>
+      <button>Login</button>
+      <a href="http://localhost:4000/auth/google"><button>Login mit Google</button></a>
+      <button>Registrieren</button>
 </>
   );
 };
